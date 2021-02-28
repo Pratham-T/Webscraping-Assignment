1635ea364e67e68bafda0b4a46f416ac57213599
Pratham <pratham071@gmail.com>
2021-03-01 01:47:08 +0530

    Created Codeforces Problem Scraper

--- Moodle/moodleLogin.py
+++ Moodle/moodleLogin.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import getpass
 
-PATH = ".\chromedriver.exe"
+PATH = "..\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://moodle.iitd.ac.in/")
